a202a10a7e8b9d2921b237d24483c6d72f562b1e
Javier Piñón <jpinon@stratio.com>
2019-11-18 15:20:25 +0100

    work in local

--- src/main/java/com/stratio/canary_testing_ms/adapter/repository/postgres/entity/ExecutionEntity.java
+++ src/main/java/com/stratio/canary_testing_ms/adapter/repository/postgres/entity/ExecutionEntity.java
@@ -2,19 +2,23 @@ package com.stratio.canary_testing_ms.adapter.repository.postgres.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.Type;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "execution_model")
+@Table(name = "execution_model", schema = "canary")
 @NoArgsConstructor
 @Getter
 @Setter
@@ -25,22 +29,33 @@ public class ExecutionEntity {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(name = "model_id", nullable = false)
+  @Column(name = "model_id")
   private String modelId;
 
-  @OneToOne
-  @JoinColumn(name = "fk_input", updatable = false, nullable = false)
-  private InputEntity inputEntity;
+  @Column(name = "n_transactions", nullable = false)
+  private Integer nTransactions;
 
-  @OneToOne
-  @JoinColumn(name = "fk_output", updatable = false, nullable = false)
-  private OutputEntity outputEntity;
+  @Column(name = "mean_amount", nullable = false)
+  private Double meanAmount;
 
-  public ExecutionEntity(String modelId,
-      InputEntity inputEntity, OutputEntity outputEntity) {
+  @Column(name = "features_ns", nullable = false)
+  @Type(type = "com.stratio.canary_testing_ms.adapter.repository.postgres.type.GenericArrayUserType")
+  private Double[] featuresNs;
+
+  @Column(name = "features", nullable = false)
+  @Type(type = "com.stratio.canary_testing_ms.adapter.repository.postgres.type.GenericArrayUserType")
+  private Double[] features;
+
+  @Column(name = "prediction", nullable = false)
+  private Integer prediction;
+
+  public ExecutionEntity(String modelId, Integer nTransactions, Double meanAmount, Double[] featuresNs,
+      Double[] features, Integer prediction) {
     this.modelId = modelId;
-    this.inputEntity = inputEntity;
-    this.outputEntity = outputEntity;
+    this.nTransactions = nTransactions;
+    this.meanAmount = meanAmount;
+    this.featuresNs = featuresNs;
+    this.features = features;
+    this.prediction = prediction;
   }
-
 }
